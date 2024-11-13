b8d59700d3ac825ec5d54be9b8cefef642388648
Johnnylongweiner <john.inez787@gmail.com>
2024-11-13 21:53:40 +0800

    Initial commit

---
+++ Assets/Abstraction/Goblin.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goblin : NPCStats
+{
+    [SerializeField] private Archer archer;
+
+    private void Start()
+    {
+        Movement();
+        TookDamage();
+        Damage (30);
+    }
+    protected override void Movement()
+    {
+        Debug.Log("Goblin moves");
+    }
+    protected override void TookDamage()
+    {
+        Debug.Log("Goblin Got punched");
+    }
+    public void Damage (int Damage)
+    {
+
+        Debug.Log("Goblin Health: " + health);
+        health -= Damage;
+        Debug.Log("Goblin new health: " + health);
+    }
+}
